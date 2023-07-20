ea100eeb92b0bcd9aee2fbf6b0f4d780ac064ff1
Swapnil Sarkar <swapnilmkab@gmail.com>
2023-07-20 06:53:37 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,59 @@
-import './App.css'
+import "./App.css";
+import { useState } from "react";
 
 export default function App() {
+  const [step, setStep] = useState(0);
+  const [counter, setCounter] = useState(0);
+
+  function stepIncrement() {
+    setStep(() => step + 1);
+  }
+
+  function stepDecrement() {
+    if (step > 0) {
+      setStep(() => step - 1);
+    }
+  }
+
+  function counterIncrement() {
+    setCounter(() => counter + step)
+  }
+
+  function counterDecrement() {
+    setCounter(() => counter - step)
+  }
+
   return (
-    <main>
-      React ⚛️ + Vite ⚡ + Replit 🌀
-    </main>
-  )
+    <div className="app">
+      <Step
+        step={step}
+        stepIncrement={stepIncrement}
+        stepDecrement={stepDecrement}
+      />
+      <Counter
+        counter={counter}
+        counterIncrement={counterIncrement}
+        counterDecrement={counterDecrement} />
+    </div>
+  );
+}
+
+function Step({ step, stepIncrement, stepDecrement }) {
+  return (
+    <div className="step">
+      <button onClick={stepDecrement}>-</button>
+      <div>Step : {step}</div>
+      <button onClick={stepIncrement}>+</button>
+    </div>
+  );
+}
+
+function Counter({ counter, counterIncrement, counterDecrement }) {
+  return (
+    <div className="step">
+      <button onClick={counterDecrement}>-</button>
+      <div>Counter : {counter}</div>
+      <button onClick={counterIncrement}>+</button>
+    </div>
+  );
 }
